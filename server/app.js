3884f7ebde480894ef11728a89c23b68b96b370f
Katieli Bianca Dieter <katielidieter@hotmail.com>
2019-10-12 17:11:33 -0300

    Add cors to server

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 mongoose
   .connect("mongodb://localhost/gql-ninja", {useUnifiedTopology: true})
