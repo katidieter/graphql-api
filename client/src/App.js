1efb652e84cf94299fba710bc4a0f44f4c29e441
Katieli Bianca Dieter <katielidieter@hotmail.com>
2019-10-13 21:39:37 -0300

    Add add book component to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+import AddBook from './components/AddBook';
 import BookList from './components/BookList';
 
 const client = new ApolloClient({
@@ -14,6 +15,7 @@ function App() {
       <div id="main">
         <h1>Ninja's Reading List</h1>
         <BookList/>
+        <AddBook/>
       </div>
     </ApolloProvider>
   );
